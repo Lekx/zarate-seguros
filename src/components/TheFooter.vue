29308671383c79addb28db634495a9cd0bbc8154
“Saúl <link_mx@live.com>
2023-05-09 22:22:16 -0600

    footer completed and color palette added

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,21 +1,30 @@
 <template>
     <v-footer app>
         <v-container>
-            <v-row class="text-center">
+            <v-row>
                 <v-col>
-                    address
+                    <h3 class="m-5">Télefonos:</h3>
+                    <p>3854-9522</p>
+                    <p>3853-8973</p>
                 </v-col>
-                <v-col>
-                    copyright
+                <v-col  class="text-center">
+                    <h3 class="m-5"><v-icon icon="mdi-copyright"></v-icon> 2023</h3>
+                    <p>
+                        <a href="https://webrocket.services" target="_blank" class="wrs-link">Web Rocket Services</a>
+                    </p>
                 </v-col>
-                <v-col>
-                    contact data
+                <v-col  class="text-right">
+                    <h3 class="m-5">Dirección:</h3>
+                    <p>Jesus García #1276-1</p>
+                    <p>Col. Villaseñor. C.P. 44260</p>
+                    <p>Guadalajara, Jalisco</p>
                 </v-col>
             </v-row>
         </v-container>
     </v-footer>
 </template>
 
-<script setup lang="ts">
-//
-</script>
+<script setup lang="ts"></script>
+<style lang="scss" scoped>
+  @import "@/styles/footer.scss";
+</style>
